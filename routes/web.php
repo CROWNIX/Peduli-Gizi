9dfadd0a9bbda6cd81d57b5d66834e9cf6df3550
CROWNIX <rahmatfauzi841@gmail.com>
2022-10-08 16:09:40 +0700

    Update RecipeSeeder dan update route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\FamilyRecipeController;
 use App\Http\Controllers\FoodRecordController;
+use App\Http\Controllers\RecipeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\AdminAgeCategoryController;
@@ -26,24 +27,26 @@ use App\Http\Controllers\AdminDashboardController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/register', [RegisterController::class, 'index']);
-Route::get('/login', [LoginController::class, 'index']);
+Route::get("/", [HomeController::class, "index"]);
+Route::get("/register", [RegisterController::class, "index"]);
+Route::get("/login", [LoginController::class, "index"]);
 
 Route::resources([
-    'profile' => ProfileController::class,
-    'food-record' => FoodRecordController::class,
-    'family-recipe' => FamilyRecipeController::class,
+    "profile" => ProfileController::class,
+    "food-records" => FoodRecordController::class,
+    "family-recipes" => FamilyRecipeController::class,
+    "recipes" => RecipeController::class,
 ]);
 
-Route::prefix('admin')->group(function () {
-    Route::get('/', [AdminHomeController::class, 'index']);
-    Route::get('/dashboard', [AdminDashboardController::class, 'index']);
+//* Route Admin 
+Route::prefix("admin")->group(function () {
+    Route::get("/", [AdminHomeController::class, "index"]);
+    Route::get("/dashboard", [AdminDashboardController::class, "index"]);
     Route::resources([
-        'age-category' => AdminAgeCategoryController::class,
-        'need-category' => AdminNeedCategoryController::class,
-        'need-sub-category' => AdminNeedSubCategoryController::class,
-        'recipe' => PostContAdminRecipeControllerroller::class,
+        "age-categories" => AdminAgeCategoryController::class,
+        "need-categories" => AdminNeedCategoryController::class,
+        "need-sub-categories" => AdminNeedSubCategoryController::class,
+        "recipes" => AdminRecipeController::class,
     ]);
 
 });
