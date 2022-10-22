bf9e10e69941aa2656db76b78fd6d1cbbe55da9d
sahrulprograming <sahrulsaefulah@gmail.com>
2022-10-22 20:35:09 +0700

    menambahkan button login & merubah setting Google Auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -99,7 +99,7 @@
                                     aria-current="page">beranda</a>
                             </li>
                             <li>
-                                <a href="#"
+                                <a href="/food-records"
                                     class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#F58634] md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">kebutuhan
                                     gizi</a>
                             </li>
@@ -111,6 +111,12 @@
                             <li>
                                 <a href="#"
                                     class="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#F58634] md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">kontak</a>
+                            </li>
+                            <li>
+                                <a href="/login"
+                                    class="outline outline-[#F58634] text-[#F58634] capitalize bg-white font-[Poppins] font-bold text-xl duration-500 px-8 py-1 mx-4 hover:bg-[#F58634] hover:text-white hover:outline-white rounded-full cursor-pointer">
+                                    masuk / daftar
+                                </a>
                             </li>
                         </ul>
                     </div>
