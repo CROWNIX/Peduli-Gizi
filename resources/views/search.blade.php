b17c5eca9c5b8bef309c98ed08769b9aa176100e
CROWNIX <rahmatfauzi841@gmail.com>
2022-10-23 23:09:14 +0700

    Integrasi resep makanan pada search

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -13,7 +13,7 @@
                 </div>
                 <input type="search" id="default-search"
                     class="block p-4 pl-12 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Cari Gizi, Artikel, Resep.." required="">
+                    placeholder="Cari Gizi, Artikel, Resep.." required="" name="search">
             </div>
         </form>
     </section>
@@ -28,44 +28,27 @@
         </div>
         <div class="swiper resep-makanan my-2">
             <div class="swiper-wrapper">
+                @foreach ($recipes as $recipe)
                 <div
                     class=" swiper-slide rounded-xl bg-gradient-to-b p-[3px] from-[#F58634] to-[#69B550] shadow-lg cursor-pointer hover:from-[#69B550] hover:to-[#F58634] duration-200 hover:scale-95">
                     <div class="flex flex-col justify-between h-full bg-white rounded-lg p-2">
-                        <img src="{{ asset('images/produk/sego tumpang 1.png') }}" alt="produk" class="w-full">
+                        <img src="{{ asset('images/produk/' . $recipe->image) }}" alt="produk" class="w-full">
                         <div class="title text-base font-bold text-black my-2">
-                            Title
+                            {{ $recipe->title }}
                         </div>
                         <p class="text-xs font-extralight text-black">Gizi per porsi</p>
                         <div class="grid grid-cols-2 gap-0">
-                            <p class="text-2xs font-normal my-0 text-black">Energi : 141 kalori
+                            <p class="text-2xs font-normal my-0 text-black">Energi : {{ $recipe->calory }} kalori
                             </p>
-                            <p class="text-2xs font-normal my-0 text-black">Protein : 2,8 g</p>
-                            <p class="text-2xs font-normal my-0 text-black">Lemak : 14,9 g</p>
+                            <p class="text-2xs font-normal my-0 text-black">Protein : {{ $recipe->protein }} g</p>
+                            <p class="text-2xs font-normal my-0 text-black">Lemak : {{ $recipe->fat }} g</p>
                             <p class="text-2xs font-normal my-0 text-black">Karbohidrat :
-                                9,2 g
+                                {{ $recipe->carboydrate }} g
                             </p>
                         </div>
                     </div>
-                </div>
-                <div
-                    class=" swiper-slide rounded-xl bg-gradient-to-b p-[3px] from-[#F58634] to-[#69B550] shadow-lg cursor-pointer hover:from-[#69B550] hover:to-[#F58634] duration-200 hover:scale-95">
-                    <div class="flex flex-col justify-between h-full bg-white rounded-lg p-2">
-                        <img src="{{ asset('images/produk/sego tumpang 1.png') }}" alt="produk" class="w-full">
-                        <div class="title text-base font-bold text-black my-2">
-                            Title
-                        </div>
-                        <p class="text-xs font-extralight text-black">Gizi per porsi</p>
-                        <div class="grid grid-cols-2 gap-0">
-                            <p class="text-2xs font-normal my-0 text-black">Energi : 141 kalori
-                            </p>
-                            <p class="text-2xs font-normal my-0 text-black">Protein : 2,8 g</p>
-                            <p class="text-2xs font-normal my-0 text-black">Lemak : 14,9 g</p>
-                            <p class="text-2xs font-normal my-0 text-black">Karbohidrat :
-                                9,2 g
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                </div>    
+                @endforeach
             </div>
         </div>
     </section>
